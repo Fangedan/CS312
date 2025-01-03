ebf15e025bb37499084b6c4a09004ee67afbb906
McAndrewJr <89644316+Fangedan@users.noreply.github.com>
2025-01-03 01:22:25 -0600

    Created Hello.java and Hello.out for the Hello World assignment.

---
+++ 00 Hello World/Hello.java
@@ -0,0 +1,24 @@
+ /**
+ * CS312 Assignment 0.
+ *
+ * Replace <NAME> with your name, stating on your honor you completed this
+ * assignment on your own and that you didn't share your code with other
+ * students.
+ * 
+ * On my honor, Andrew Lin, this programming assignment is my own work and I have
+ * not shared my solution with any other student in the class.
+ *
+ * A program to print out Hello World.
+ *
+ *  email address: alin257274@gmail.com
+ *  UTEID: al58444
+ *  Number of slip days I am using on this assignment: ?
+ */
+
+public class Hello {
+
+    // Print "Hello World!" to the console.
+    public static void main(String[] args) {
+		System.out.print("Hello World!");
+    }
+}
